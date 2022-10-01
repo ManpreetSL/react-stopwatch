4541076dd3af43f13185fd8f2e2195e4da4ef6e7
Manpreet Singh Lalria <manpreet.lalria@vanti.co.uk>
2022-10-01 18:02:51 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Vaheguru Jee Kaa Khalsa; Vaheguru Jee Kee Fateh!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
