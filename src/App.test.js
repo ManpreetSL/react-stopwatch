741d59ded69eccfeebd89799382a802d755b1a6f
Manpreet Singh Lalria <manpreet.lalria@vanti.co.uk>
2022-10-02 07:38:14 +0100

    Added a basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
+/**
+ * render: lets us render the component as React would
+ * screen: a utility for finding elements the same way the user does
+ */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('says GurFateh', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading')).toHaveTextContent('Vaheguru Jee Kaa Khalsa; Vaheguru Jee Kee Fateh!');
 });
